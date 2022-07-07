5242c11c4fb7d35413fcaf042853b5ca75682533
Jesus Ciriano <jciriano@nalandaglobal.com>
2022-07-07 10:59:27 +0200

    Dominio y persistencia

---
+++ src/main/java/com/polarbookshop/catalogservice/persistence/BookRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.polarbookshop.catalogservice.persistence;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ConcurrentReferenceHashMap;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+
+    private static final Map<String, Book> books = new ConcurrentReferenceHashMap<>();
+    @Override
+    public Iterable<Book> findAll() {
+        return books.values();
+    }
+
+    @Override
+    public Optional<Book> findByIsbn(String isbn) {
+        return existsByIsbn(isbn) ? Optional.of(books.get(isbn)) : Optional.empty();
+    }
+
+    @Override
+    public boolean existsByIsbn(String isbn) {
+        return books.get(isbn) != null;
+    }
+
+    @Override
+    public Book saveBook(Book book) {
+        books.put(book.isbn(), book);
+        return book;
+    }
+
+    @Override
+    public void deleteByIsbn(String isbn) {
+        books.remove(isbn);
+    }
+}
