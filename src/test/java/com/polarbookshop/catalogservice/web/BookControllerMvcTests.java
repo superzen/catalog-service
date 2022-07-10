f52094105a5705ea09785d4c8cac96679e2440f4
Jesus Ciriano <jciriano@nalandaglobal.com>
2022-07-10 17:26:49 +0200

    Integration test with @WebMvcTest

--- src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,19 +15,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(BookController.class)
 class BookControllerMvcTests {
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @MockBean
-  private BookService bookService;
+    @MockBean
+    private BookService bookService;
 
-  @Test
-  void whenGetBookNotExistingThenShouldReturn404() throws Exception {
-    String isbn = "73737313940";
-    given(bookService.viewBookDetails(isbn))
-      .willThrow(BookNotFoundException.class);
-    mockMvc
-      .perform(get("/books/" + isbn))
-      .andExpect(status().isNotFound());
-  }
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "73737313940";
+        given(bookService.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
 }
