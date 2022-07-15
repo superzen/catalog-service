6eb981b46acb5cff58f18dc1c509c2863484453f
Jesus Ciriano <jciriano@nalandaglobal.com>
2022-07-15 12:58:21 +0200

    DEFINING TRANSACTIONAL CONTEXTS

--- src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -3,6 +3,7 @@ package com.polarbookshop.catalogservice.domain;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ public interface BookRepository extends CrudRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
 
     @Modifying
+    @Transactional
     @Query("delete from Book where isbn = :isbn")
     void deleteByIsbn(String isbn);
 
